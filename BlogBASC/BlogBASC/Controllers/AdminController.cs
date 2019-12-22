6d27547e4dfb8fa2ab882c9e5fc07fa0c9f2e298
Senaca <35538900+Senaca@users.noreply.github.com>
2019-12-22 03:56:09 +0300

    Admin Paneli yapıldı

---
+++ BlogBASC/BlogBASC/Controllers/AdminController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BlogBASC.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Bloglar()
+        {
+            return View();
+        }
+        public ActionResult Kategoriler()
+        {
+            return View();
+        }
+
+    }
+}
